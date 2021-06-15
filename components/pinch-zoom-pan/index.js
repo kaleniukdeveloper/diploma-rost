9f0960e63e3e00ab8a1d6e9b9d5db5441cf63f47
kaleniukdeveloper <kaleniuk.developer@gmail.com>
2021-06-15 18:02:15 +0300

    update

--- components/pinch-zoom-pan/index.js
+++ components/pinch-zoom-pan/index.js
@@ -1,37 +1,37 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+// import { useCallback, useEffect, useRef, useState } from "react";
 
-import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
+// import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
 
 export default ({ areaRef, productionMode, children }) => {
-  const pinchZoomRef = useRef(),
-    [width, setWidth] = useState(0),
-    onUpdate = useCallback(({ x, y, scale }) => {
-      const { current: area } = areaRef;
+  //   const pinchZoomRef = useRef(),
+  //     [width, setWidth] = useState(0),
+  //     onUpdate = useCallback(({ x, y, scale }) => {
+  //       const { current: area } = areaRef;
 
-      if (area) {
-        const value = make3dTransformValue({ x, y, scale });
+  //       if (area) {
+  //         const value = make3dTransformValue({ x, y, scale });
 
-        area.style.setProperty("transform", value);
-      }
-    }, []);
+  //         area.style.setProperty("transform", value);
+  //       }
+  //     }, []);
 
-  useEffect(() => {
-    pinchZoomRef.current?.scaleTo({ x: 0, y: 0, scale: 1 });
-  }, [productionMode]);
+  //   useEffect(() => {
+  //       pinchZoomRef.current?.scaleTo({ x: 0, y: 0, scale: 1 });
+  //     }, [productionMode]);
 
-  useEffect(() => {}, []);
-
-  return (
-    <QuickPinchZoom
-      ref={pinchZoomRef}
-      maxZoom={5}
-      minZoom={0.5}
-      draggableUnzoomed={true}
-      onUpdate={onUpdate}
-      lockDragAxis={false}
-      inertia={false}
-    >
-      {children}
-    </QuickPinchZoom>
-  );
+  //   useEffect(() => {}, []);
+  return children;
+  //   return (
+  //     <QuickPinchZoom
+  //       ref={pinchZoomRef}
+  //       maxZoom={5}
+  //       minZoom={0.5}
+  //       draggableUnzoomed={true}
+  //       onUpdate={onUpdate}
+  //       lockDragAxis={false}
+  //       inertia={false}
+  //     >
+  //       {children}
+  //     </QuickPinchZoom>
+  //   );
 };
