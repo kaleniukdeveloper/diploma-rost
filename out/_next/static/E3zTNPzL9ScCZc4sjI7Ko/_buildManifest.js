20720896dbc4e2cab9d76b1ba10b67b3e9c2e42f
kaleniukdeveloper <kaleniuk.developer@gmail.com>
2021-06-15 16:20:09 +0300

    add export

---
+++ out/_next/static/E3zTNPzL9ScCZc4sjI7Ko/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/693-293a8d5daab8095b14ea.js","static/chunks/pages/index-cd3be2ec499344dea06a.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
